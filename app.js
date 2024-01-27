9092e6faf969beda13171d8e63e77f8e57d439e5
Faizan <mohammed.f@perfectiongeeks.com>
2024-01-27 09:37:18 +0530

    working

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer');
+
 console.log('puppeteer started');
 
-console.log('Before');
-const browserOpenPromise = puppeteer.launch({headless : false});
-
-browserOpenPromise.then(() => {
+(async () => {
+    console.log('Before');
+    const browser = await puppeteer.launch({headless : false, ignoreHTTPSErrors: true});
     console.log("Browser Opened");
-});
 
+    const page = await browser.newPage();
+    await page.goto('http://secure.indeed.com/');
+    console.log("Navigated to http://indeed.in");
+
+    // Rest of your code
+})();
 console.log("After");
